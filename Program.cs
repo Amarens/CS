d0accdd7ab1f73d6932edd9e37e1444a020b208f
Amarens <54943995+Amarens@users.noreply.github.com>
2020-07-07 04:07:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MakingProgress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare steps variable
+
+            int steps = 0;
+
+            // Two steps forward 
+
+            steps += 2;
+
+            // One step back 
+
+            steps -= 1;
+
+            // Print result to the console
+
+            Console.WriteLine(steps);
+
+            Console.WriteLine("Above is the amount of steps.");
+
+
+
+        }
+    }
+}
